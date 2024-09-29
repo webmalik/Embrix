fc2ca8d9ac121a1cac3e33068b223e7d8ad36240
Dmytro Adamovych <dmytro.adamovych@gmail.com>
2024-09-29 21:54:13 +0300

    Start mobile

---
+++ scripts/main-page/reviews-swiper.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".reviews__swiper", {
+    effect: "cards",
+    grabCursor: true,
+    navigation: {
+        nextEl: '.reviews__swiper-button-prev',
+        prevEl: '.reviews__swiper-button-next',
+      },
+  });
